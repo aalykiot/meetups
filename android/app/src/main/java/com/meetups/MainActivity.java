686b746b078414d7c50b82972186bada081092d4
Alex Alikiotis <alexalikiotis5@gmail.com>
2019-02-15 00:29:03 +0200

    Fixing android production bundle bug

--- android/app/src/main/java/com/meetups/MainActivity.java
+++ android/app/src/main/java/com/meetups/MainActivity.java
@@ -1,7 +1,6 @@
 package com.meetups;
 
 import com.facebook.react.ReactActivity;
-import com.calendarevents.CalendarEventsPackage;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,11 +12,4 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "Meetups";
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        CalendarEventsPackage.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-}   
+}
