ac5669766369bd587988010b326e8564688726a2
Alex Alikiotis <alexalikiotis5@gmail.com>
2019-02-14 20:09:52 +0200

    Fixing android js transpiling bug

--- index.js
+++ index.js
@@ -3,6 +3,15 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
+// symbol polyfills
+global.Symbol = require('core-js/es6/symbol');
+require('core-js/fn/symbol/iterator');
+
+// collection fn polyfills
+require('core-js/fn/map');
+require('core-js/fn/set');
+require('core-js/fn/array/find');
+
 import { AppRegistry } from 'react-native';
 
 import App from './src/App';
